5d68d21ca7cb11b05d0e4a12ff95695932c7c471
YahirSolar <yahirsolar1@gmail.com>
2023-03-03 19:37:40 -0600

    Clase Casa

---
+++ Casa.cs
@@ -0,0 +1,23 @@
+namespace Tarea2;
+
+class Casa {
+    public int area;
+    public Puerta puerta = new Puerta();
+
+public Casa(int area) {
+    this.area = area;
+    this.puerta = new Puerta();
+}
+
+public void SetArea(int area) {
+    this.area = area;
+}
+
+public void SetPuerta(Puerta puerta) {
+    this.puerta = puerta;
+}
+
+public void MostrarDatos() { 
+    Console.WriteLine("Soy una casa, mi area es " + this.area + " m2");
+}
+}
