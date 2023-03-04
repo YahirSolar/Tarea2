95e888294077434850c93249055e7018bb9a9dbd
YahirSolar <yahirsolar1@gmail.com>
2023-03-03 21:47:22 -0600

    Clase Apartamento

---
+++ Apartamento.cs
@@ -0,0 +1,12 @@
+namespace Tarea2;
+
+class Apartamento : Casa {
+
+    public Apartamento(): base(50) {
+    }
+
+    new public void MostrarDatos() {
+        Console.WriteLine("Soy un Apartamento, mi area es " + this.area + " m2");
+    }
+}
+
