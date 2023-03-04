2a2338368fdc4b8f4a501e517a2af2c8f5a49f24
YahirSolar <yahirsolar1@gmail.com>
2023-03-03 19:19:02 -0600

    Clase Puerta

---
+++ Puerta.cs
@@ -0,0 +1,18 @@
+namespace Tarea2;
+
+class Puerta {
+    public string color = "";
+
+public Puerta() {
+    this.color = "Café";
+} 
+
+public void SetColor(string color) {
+    this.color = color;
+}
+
+public void MostrarDatos() {
+    Console.WriteLine("Soy una Puerta, mi color es " + this.color);
+}
+
+}
